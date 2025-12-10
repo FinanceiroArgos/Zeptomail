c3bee0701a4a81e0399cc3472cf6ad26d093b433
FinanceiroArgos <financeiroargospromotora@gmail.com>
2025-12-10 15:05:09 -0300

    Atualização do webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,15 +2,30 @@ import { prisma } from "@/lib/prisma";
 import { revalidateTag } from "next/cache";
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const request_id = body.event_message[0].request_id
+  try {
+    const body = await req.json();
+    const request_id = body.event_message[0].request_id
 
-  // Atualizar o status do email na base de dados
-  await prisma.email.update({
-    where: { id: request_id }, data: {
-      status: 'Erro'
-    }
-  })
+    // Verificar se recebeu o request_id
+    if (!request_id) return new Response("Faltando request_id", { status: 400 });
 
-  return new Response("ok", { status: 200 });
+    // Tenta buscar o email
+    const email = await prisma.email.findUnique({
+      where: { id: request_id }
+    });
+
+    if (!email) return new Response("Email não encontrado", { status: 200 });
+
+    // Atualizar o status do email na base de dados
+    await prisma.email.update({
+      where: { id: request_id },
+      data: { status: 'Erro' }
+    })
+
+    return new Response("ok", { status: 200 });
+  } catch (e) {
+    return new Response("internal error", { status: 500 });
+  }
+
+
 }
